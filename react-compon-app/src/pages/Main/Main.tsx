8e189b93ccbda638a56ab80ac5d0c3d14896f736
Anubic29 <asitailo2039@gmail.com>
2023-03-18 14:54:54 +0200

    feat: add search-bar to main page

--- react-compon-app/src/pages/Main/Main.tsx
+++ react-compon-app/src/pages/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
 import styles from './Main.module.scss';
 
@@ -6,7 +7,13 @@ function Main() {
   return (
     <div className={styles['main']}>
       <div className={styles['content']}>
-        <h1>Main</h1>
+        <div className={styles['search-container']}>
+          <h1 className={styles['title']}>Search:</h1>
+          <div className={styles['search']}>
+            <MdSearch className={styles['search__icon']} />
+            <input type="text" className={styles['search__input']} />
+          </div>
+        </div>
       </div>
     </div>
   );
