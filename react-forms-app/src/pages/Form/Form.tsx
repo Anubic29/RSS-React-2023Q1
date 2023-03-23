e402f4f35d448db4c51104536762887ca977d71b
Anubic29 <asitailo2039@gmail.com>
2023-03-23 20:12:41 +0200

    feat: add inputs

--- react-forms-app/src/pages/Form/Form.tsx
+++ react-forms-app/src/pages/Form/Form.tsx
@@ -12,19 +12,82 @@ export default class Form extends Component<{}, FormState> {
     value: '',
   };
 
+  onSubmitHandler(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log('Submit');
+  }
+
   render() {
     return (
-      <div>
-        <form>
-          <div className={styles['input-block']}>
-            <label htmlFor=""></label>
-            <input type="text" />
-          </div>
-          <div className={styles['input-block']}>
-            <label htmlFor=""></label>
-            <input type="text" />
-          </div>
-        </form>
+      <div className={styles['form-page']}>
+        <div className={styles['content']}>
+          <form className={styles['form']} onSubmit={this.onSubmitHandler}>
+            <div className={styles['input-block']}>
+              <label className={styles['label']} htmlFor="user-name">
+                Username
+              </label>
+              <input className={styles['input']} type="text" id="user-name" />
+            </div>
+            <div className={styles['input-block']}>
+              <label className={styles['label']} htmlFor="date">
+                Birthday
+              </label>
+              <input className={styles['input']} type="date" id="date" />
+            </div>
+            <div className={styles['input-block']}>
+              <label className={styles['label']} htmlFor="select">
+                Country
+              </label>
+              <select className={`${styles['input']} ${styles['select']}`} id="select">
+                <option value="" disabled={true}>
+                  Select country
+                </option>
+                <option value="Ukraine">Ukraine</option>
+                <option value="USA">USA</option>
+                <option value="Mexico">Mexico</option>
+                <option value="Spain">Spain</option>
+                <option value="France">France</option>
+              </select>
+            </div>
+            <div className={styles['input-block']}>
+              <p className={styles['label']}>Skills</p>
+              <label className={styles['check-block']}>
+                <input className={styles['input']} type="checkbox" /> HTML
+              </label>
+              <label className={styles['check-block']}>
+                <input className={styles['input']} type="checkbox" /> CSS
+              </label>
+              <label className={styles['check-block']}>
+                <input className={styles['input']} type="checkbox" /> JavaScript
+              </label>
+              <label className={styles['check-block']}>
+                <input className={styles['input']} type="checkbox" /> React
+              </label>
+            </div>
+            <div className={styles['input-block']}>
+              <p className={styles['label']}>Type</p>
+              <div className={styles['switch-block']}>
+                <p>Basic</p>
+                <label className={styles['switch']}>
+                  <input type="checkbox" />
+                  <span className={styles['slider']}></span>
+                </label>
+                <p>Premium</p>
+              </div>
+            </div>
+            <div className={styles['input-block']}>
+              <label className={styles['label']} htmlFor="file">
+                File
+              </label>
+              <input className={styles['input']} type="file" id="file" />
+            </div>
+            <div className={styles['btn-block']}>
+              <button className={styles['button']} type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
