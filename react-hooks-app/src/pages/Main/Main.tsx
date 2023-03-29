23897efa1a6b8b6116ac0b5558d06d7b6a2db245
Anubic29 <asitailo2039@gmail.com>
2023-03-29 11:08:41 +0300

    feat: replace class component Main with function component

--- react-hooks-app/src/pages/Main/Main.tsx
+++ react-hooks-app/src/pages/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { CardList } from './components';
 import { SearchBar } from '../../components/';
 
@@ -6,39 +6,28 @@ import { cards } from '../../fakeData/cards';
 
 import styles from './Main.module.scss';
 
-interface MainState {
-  searchValue: string;
-}
+function Main() {
+  const [searchValue, setSearchValue] = useState('');
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-export default class Main extends Component<{}, MainState> {
-  state = {
-    searchValue: '',
-  };
-
-  onChangeSearchHandler = (value: string) => {
-    this.setState({ searchValue: value });
-  };
-
-  render() {
-    return (
-      <div className={styles['main']} data-testid="main-page">
-        <div className={styles['content']}>
-          <div className={styles['search-container']}>
-            <h1 className={styles['title']}>Search:</h1>
-            <SearchBar onChangeHandler={this.onChangeSearchHandler} />
-          </div>
-          <div className={styles['card-list-block']}>
-            <CardList
-              cards={cards.filter(
-                (card) =>
-                  card.title.includes(this.state.searchValue) ||
-                  card.genreList.some((genre) => genre.includes(this.state.searchValue))
-              )}
-            />
-          </div>
+  return (
+    <div className={styles['main']} data-testid="main-page">
+      <div className={styles['content']}>
+        <div className={styles['search-container']}>
+          <h1 className={styles['title']}>Search:</h1>
+          <SearchBar onChangeHandler={(value: string) => setSearchValue(value)} />
+        </div>
+        <div className={styles['card-list-block']}>
+          <CardList
+            cards={cards.filter(
+              (card) =>
+                card.title.includes(searchValue) ||
+                card.genreList.some((genre) => genre.includes(searchValue))
+            )}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Main;
