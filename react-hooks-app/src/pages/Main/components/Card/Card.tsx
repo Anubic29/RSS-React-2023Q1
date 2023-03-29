0f3b9ee5fe8e16b190136381ee2e2fb77f87d770
Anubic29 <asitailo2039@gmail.com>
2023-03-29 11:37:58 +0300

    feat: replace class component Card with function component

--- react-hooks-app/src/pages/Main/components/Card/Card.tsx
+++ react-hooks-app/src/pages/Main/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './Card.module.scss';
 
@@ -9,31 +9,31 @@ interface CardProps {
   price: string;
 }
 
-export default class Card extends Component<CardProps> {
-  render() {
-    return (
-      <div className={styles['card']} data-testid="game-card">
-        <div className={styles['image-container']}>
-          <img src={this.props.image} alt={this.props.title} data-testid="game-card-image" />
+function Card(props: CardProps) {
+  return (
+    <div className={styles['card']} data-testid="game-card">
+      <div className={styles['image-container']}>
+        <img src={props.image} alt={props.title} data-testid="game-card-image" />
+      </div>
+      <div className={styles['content']}>
+        <h1 className={styles['title']} data-testid="game-card-title">
+          {props.title}
+        </h1>
+        <div
+          className={styles['genres']}
+          title={props.genreList.join(', ')}
+          data-testid="game-card-genres"
+        >
+          {props.genreList.join(', ')}
         </div>
-        <div className={styles['content']}>
-          <h1 className={styles['title']} data-testid="game-card-title">
-            {this.props.title}
-          </h1>
-          <div
-            className={styles['genres']}
-            title={this.props.genreList.join(', ')}
-            data-testid="game-card-genres"
-          >
-            {this.props.genreList.join(', ')}
-          </div>
-          <div className={styles['price-block']}>
-            <div className={styles['price']} data-testid="game-card-price">
-              {this.props.price} $
-            </div>
+        <div className={styles['price-block']}>
+          <div className={styles['price']} data-testid="game-card-price">
+            {props.price} $
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Card;
