19c7008e8b613651ef2c77e5130f9fa50efabae6
Anubic29 <asitailo2039@gmail.com>
2023-03-29 17:49:51 +0300

    feat: replace class component FormCardList with function component

--- react-hooks-app/src/pages/Form/components/FormCardList/FormCardList.tsx
+++ react-hooks-app/src/pages/Form/components/FormCardList/FormCardList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormCard } from '../';
 import { FormCardType } from 'types/CardType';
 
@@ -8,31 +8,31 @@ interface FormCardListProps {
   cards: FormCardType[];
 }
 
-export default class FormCardList extends Component<FormCardListProps> {
-  render() {
-    return (
-      <div className={styles['card-list']} data-testid="card-list">
-        {this.props.cards.length > 0 ? (
-          this.props.cards.map((card, idx) => (
-            <div className={styles['card-block']} key={idx}>
-              <FormCard
-                id={idx + 1}
-                userName={card['userName']}
-                date={card['date']}
-                country={card['country']}
-                skills={card['skills']}
-                language={card['language']}
-                type={card['type']}
-                file={card['file']}
-              />
-            </div>
-          ))
-        ) : (
-          <div className={styles['empty-message']} data-testid="empty-message">
-            List is empty
+function FormCardList(props: FormCardListProps) {
+  return (
+    <div className={styles['card-list']} data-testid="card-list">
+      {props.cards.length > 0 ? (
+        props.cards.map((card, idx) => (
+          <div className={styles['card-block']} key={idx}>
+            <FormCard
+              id={idx + 1}
+              userName={card['userName']}
+              date={card['date']}
+              country={card['country']}
+              skills={card['skills']}
+              language={card['language']}
+              type={card['type']}
+              file={card['file']}
+            />
           </div>
-        )}
-      </div>
-    );
-  }
+        ))
+      ) : (
+        <div className={styles['empty-message']} data-testid="empty-message">
+          List is empty
+        </div>
+      )}
+    </div>
+  );
 }
+
+export default FormCardList;
