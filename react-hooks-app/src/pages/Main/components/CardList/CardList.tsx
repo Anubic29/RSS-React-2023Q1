eb45d97f5032556ba0a9c5e5f3ac8a28b9b7466a
Anubic29 <asitailo2039@gmail.com>
2023-03-29 11:12:24 +0300

    feat: replace class component CardList with function component

--- react-hooks-app/src/pages/Main/components/CardList/CardList.tsx
+++ react-hooks-app/src/pages/Main/components/CardList/CardList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CardType } from '../../../../types/CardType';
 import { Card } from '../';
 
@@ -8,25 +8,25 @@ interface CardListProps {
   cards: CardType[];
 }
 
-export default class CardList extends Component<CardListProps> {
-  render() {
-    return (
-      <div className={styles['card-list']} data-testid="card-list">
-        {this.props.cards.length > 0 ? (
-          this.props.cards.map((card, idx) => (
-            <div className={styles['card-block']} key={idx}>
-              <Card
-                title={card.title}
-                image={card.image}
-                genreList={card.genreList}
-                price={`${card.price}`}
-              />
-            </div>
-          ))
-        ) : (
-          <h1 data-testid="card-list-empty-message">List is empty</h1>
-        )}
-      </div>
-    );
-  }
+function CardList(props: CardListProps) {
+  return (
+    <div className={styles['card-list']} data-testid="card-list">
+      {props.cards.length > 0 ? (
+        props.cards.map((card, idx) => (
+          <div className={styles['card-block']} key={idx}>
+            <Card
+              title={card.title}
+              image={card.image}
+              genreList={card.genreList}
+              price={`${card.price}`}
+            />
+          </div>
+        ))
+      ) : (
+        <h1 data-testid="card-list-empty-message">List is empty</h1>
+      )}
+    </div>
+  );
 }
+
+export default CardList;
