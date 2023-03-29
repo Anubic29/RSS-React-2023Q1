14c2d754f0d4ac5696d20b1764afcdbee1584941
Anubic29 <asitailo2039@gmail.com>
2023-03-29 17:48:35 +0300

    feat: replace class component FormCard with function component

--- react-hooks-app/src/pages/Form/components/FormCard/FormCard.tsx
+++ react-hooks-app/src/pages/Form/components/FormCard/FormCard.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import styles from './FormCard.module.scss';
 
@@ -13,28 +13,28 @@ interface FormCardProps {
   file: string;
 }
 
-export default class FormCard extends Component<FormCardProps> {
-  render() {
-    const { id, userName, ...rest } = this.props;
+function FormCard(props: FormCardProps) {
+  const { id, userName, ...rest } = props;
 
-    return (
-      <div className={styles['card']} data-testid="form-card">
-        <div className={styles['title-block']}>
-          <h1 className={styles['title']} data-testid="form-card-title">
-            {id}. {userName}
-          </h1>
-        </div>
-        <ul className={styles['param-list']}>
-          {Object.entries(rest).map((param, idx) => (
-            <li key={idx} className={styles['param']}>
-              <span className={styles['subtitle']}>{param[0]}:</span>
-              <span className={styles['value']} data-testid={`form-card-${param[0]}`}>
-                {param[1]}
-              </span>
-            </li>
-          ))}
-        </ul>
+  return (
+    <div className={styles['card']} data-testid="form-card">
+      <div className={styles['title-block']}>
+        <h1 className={styles['title']} data-testid="form-card-title">
+          {id}. {userName}
+        </h1>
       </div>
-    );
-  }
+      <ul className={styles['param-list']}>
+        {Object.entries(rest).map((param, idx) => (
+          <li key={idx} className={styles['param']}>
+            <span className={styles['subtitle']}>{param[0]}:</span>
+            <span className={styles['value']} data-testid={`form-card-${param[0]}`}>
+              {param[1]}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default FormCard;
