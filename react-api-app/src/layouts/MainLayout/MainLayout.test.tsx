b2da011d79c6621f89d152e429ac51acdc41bf42
Anubic29 <asitailo2039@gmail.com>
2023-04-08 13:01:54 +0300

    feat: add tests for MainLayout component

--- react-api-app/src/layouts/MainLayout/MainLayout.test.tsx
+++ react-api-app/src/layouts/MainLayout/MainLayout.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './MainLayout';
+import { OverlayProvider, useOverlay } from '../../contexts';
 
 describe('MainLayout', () => {
   it('should be in document', () => {
@@ -14,5 +15,83 @@ describe('MainLayout', () => {
     expect(header).toBeInTheDocument();
     const content = screen.getByTestId('content');
     expect(content).toBeInTheDocument();
+  });
+
+  it('overlay close with overlay btn', () => {
+    const TestComponent = () => {
+      const overlay = useOverlay();
+
+      return (
+        <>
+          <MainLayout />
+          <button
+            data-testid="button-open"
+            onClick={() => {
+              overlay.setContentOverlay(<h1>Title</h1>);
+              overlay.setIsVisible(true);
+            }}
+          >
+            Open
+          </button>
+        </>
+      );
+    };
+
+    render(
+      <BrowserRouter>
+        <OverlayProvider>
+          <TestComponent />
+        </OverlayProvider>
+      </BrowserRouter>
+    );
+
+    const btnOpen = screen.getByTestId('button-open');
+    fireEvent.click(btnOpen);
+
+    const overlay = screen.getByTestId('overlay');
+    const overlayClose = screen.getByTestId('overlay-close');
+    expect(overlay).toBeInTheDocument();
+
+    fireEvent.click(overlayClose);
+    expect(overlay).not.toBeInTheDocument();
+  });
+
+  it('overlay close with overlay back', () => {
+    const TestComponent = () => {
+      const overlay = useOverlay();
+
+      return (
+        <>
+          <MainLayout />
+          <button
+            data-testid="button-open"
+            onClick={() => {
+              overlay.setContentOverlay(<h1>Title</h1>);
+              overlay.setIsVisible(true);
+            }}
+          >
+            Open
+          </button>
+        </>
+      );
+    };
+
+    render(
+      <BrowserRouter>
+        <OverlayProvider>
+          <TestComponent />
+        </OverlayProvider>
+      </BrowserRouter>
+    );
+
+    const btnOpen = screen.getByTestId('button-open');
+    fireEvent.click(btnOpen);
+
+    const overlay = screen.getByTestId('overlay');
+    const overlayBack = screen.getByTestId('overlay-back');
+    expect(overlay).toBeInTheDocument();
+
+    fireEvent.click(overlayBack);
+    expect(overlay).not.toBeInTheDocument();
   });
 });
