2afc473d50abc731bf1060eca9768c4749db57e4
Anubic29 <asitailo2039@gmail.com>
2023-04-08 15:09:41 +0300

    feat: add mock handlers for server and add some tests for Main component

---
+++ react-api-app/src/mocks/handlers.ts
@@ -0,0 +1,57 @@
+import { rest } from 'msw';
+
+const characterArr = [
+  {
+    created: '2017-11-04T18:48:46.250Z',
+    episode: ['https://rickandmortyapi.com/api/episode/1'],
+    gender: 'Male',
+    id: 1,
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    location: {
+      name: 'Citadel of Ricks',
+      url: 'https://rickandmortyapi.com/api/location/3',
+    },
+    name: 'Rick Sanchez',
+    origin: { name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1' },
+    species: 'Human',
+    status: 'Alive',
+    type: '',
+    url: 'https://rickandmortyapi.com/api/character/1',
+  },
+  {
+    created: '2017-11-04T20:33:30.779Z',
+    episode: ['https://rickandmortyapi.com/api/episode/31'],
+    gender: 'unknown',
+    id: 13,
+    image: 'https://rickandmortyapi.com/api/character/avatar/13.jpeg',
+    location: {
+      name: 'Earth (Replacement Dimension)',
+      url: 'https://rickandmortyapi.com/api/location/20',
+    },
+    name: 'Alien Googah',
+    origin: { name: 'unknown', url: '' },
+    species: 'Alien',
+    status: 'unknown',
+    type: '',
+    url: 'https://rickandmortyapi.com/api/character/13',
+  },
+];
+
+export const handlers = [
+  rest.get('https://rickandmortyapi.com/api/character', (req, res, ctx) => {
+    const characterName = req.url.searchParams.get('name');
+    const filteredArr = characterArr.filter((character) =>
+      character.name.includes(characterName ? characterName : '')
+    );
+    if (filteredArr.length > 0) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          results: characterArr,
+        })
+      );
+    } else {
+      return res(ctx.status(500));
+    }
+  }),
+];
--- react-api-app/src/pages/Main/Main.test.tsx
+++ react-api-app/src/pages/Main/Main.test.tsx
@@ -2,7 +2,41 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './Main';
-import { cards } from '../../fakeData/cards';
+// import { CharacterType } from 'types/CharacterType';
+
+// const mockCardListData: CharacterType[] = [
+//   {
+//     created: '2017-11-04T18:48:46.250Z',
+//     episode: ['https://rickandmortyapi.com/api/episode/1'],
+//     gender: 'Male',
+//     id: 1,
+//     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+//     location: { name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3' },
+//     name: 'Rick Sanchez',
+//     origin: { name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1' },
+//     species: 'Human',
+//     status: 'Alive',
+//     type: '',
+//     url: 'https://rickandmortyapi.com/api/character/1',
+//   },
+//   {
+//     created: '2017-11-04T20:33:30.779Z',
+//     episode: ['https://rickandmortyapi.com/api/episode/31'],
+//     gender: 'unknown',
+//     id: 13,
+//     image: 'https://rickandmortyapi.com/api/character/avatar/13.jpeg',
+//     location: {
+//       name: 'Earth (Replacement Dimension)',
+//       url: 'https://rickandmortyapi.com/api/location/20',
+//     },
+//     name: 'Alien Googah',
+//     origin: { name: 'unknown', url: '' },
+//     species: 'Alien',
+//     status: 'unknown',
+//     type: '',
+//     url: 'https://rickandmortyapi.com/api/character/13',
+//   },
+// ];
 
 describe('Main', () => {
   it('should be in document', () => {
@@ -22,36 +56,28 @@ describe('Main', () => {
     expect(cardList).toBeInTheDocument();
   });
 
-  it('filters cards based on search value', () => {
-    render(
+  it('correct data get from api', async () => {
+    const { findAllByTestId } = render(
       <BrowserRouter>
         <Main />
       </BrowserRouter>
     );
 
-    const genreStr = 'Strategy';
-    const input = screen.getByTestId('search-bar-input');
-    fireEvent.change(input, { target: { value: genreStr } });
-    const cardsInList = screen.getAllByTestId('game-card');
-    const filteredCards = cards.filter(
-      (card) =>
-        card.title.includes(genreStr) || card.genreList.some((genre) => genre.includes(genreStr))
-    );
-
-    expect(cardsInList).toHaveLength(filteredCards.length);
+    expect((await findAllByTestId('character-card')).length).toEqual(2);
   });
 
-  it('filter cards based on random value', () => {
-    render(
+  it('filter cards based on random value', async () => {
+    const { findByTestId } = render(
       <BrowserRouter>
         <Main />
       </BrowserRouter>
     );
 
     const input = screen.getByTestId('search-bar-input');
-    fireEvent.change(input, { target: { value: 'StrategyArmoryDesert' } });
+    fireEvent.change(input, { target: { value: 'asdqwerty' } });
+    fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
 
-    const emptyListMessage = screen.getByTestId('card-list-empty-message');
-    expect(emptyListMessage).toBeInTheDocument();
+    const emptyListMessage = await findByTestId('card-list-empty-message');
+    expect(emptyListMessage).toBeDefined();
   });
 });
