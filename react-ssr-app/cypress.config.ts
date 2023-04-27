17f1721059097b832412035c1973e6767f41ca8a
Anubic29 <asitailo2039@gmail.com>
2023-04-27 21:17:10 +0300

    feat: addd coverage for e2e tests

--- react-ssr-app/cypress.config.ts
+++ react-ssr-app/cypress.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'cypress';
+import codeCoverageTask from '@cypress/code-coverage/task';
 
 export default defineConfig({
+  env: {
+    codeCoverage: {
+      exclude: 'cypress/**/*.*',
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      codeCoverageTask(on, config);
+      return config;
     },
     baseUrl: 'http://localhost:8000',
   },
